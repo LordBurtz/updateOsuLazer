347261d493bcce9bea4e174eff5df6601951b07d
LordBurtz <fridolin.karger@gmail.com>
2021-12-12 20:28:37 +0100

    python script ready

--- fetchLatest.py
+++ fetchLatest.py
@@ -3,7 +3,9 @@
 import requests
 import json 
 import sys
+import subprocess
 
+SCRIPT='/home/fridolin/bin/updateOsuLazer/updateOsu.sh'
 url = 'https://api.github.com/repos/ppy/osu/releases/latest'
 
 respone = json.loads(requests.get(url))
@@ -12,4 +14,7 @@ with open('current_version.xbash', 'rw') as f:
     version = file.read()
 
 if version != response['tag_name']:
-
+    print(subprocess.run([SCRIPT, response['tag_name'], capture_output=True))
+    with open('current_version.xbash', 'w') as f:
+        f.write(version)
+print('osu!Lazer updated successfully')
