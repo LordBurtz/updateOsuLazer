c7f577a973f24b04c71c76da3a1cc15ecd8f7e02
LordBurtz <fridolin.karger@gmail.com>
2021-12-12 20:34:44 +0100

    more typo fixing (3 last commits

--- fetchLatest.py
+++ fetchLatest.py
@@ -8,9 +8,9 @@ import subprocess
 SCRIPT='/home/fridolin/bin/updateOsuLazer/updateOsu.sh'
 url = 'https://api.github.com/repos/ppy/osu/releases/latest'
 
-respone = requests.get(url)
+response = requests.get(url)
 
-json_data = respone.json() if response and response.status_code == 200 else sys.exit('Internet connection be misbehaving')
+json_data = response.json() if response and response.status_code == 200 else sys.exit('Internet connection be misbehaving')
 
 with open('current_version.xbash', 'rw') as f:
     version = file.read()
